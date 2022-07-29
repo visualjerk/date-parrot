545c145ddf293ae65215fa4bfc3ff27b5ac3c4c8
visualjerk <jb@visualjerk.de>
2022-07-29 16:29:24 +0200

    parse days and weeks

--- src/parse.ts
+++ src/parse.ts
@@ -1,13 +1,107 @@
-export function parse(input: string) {
-  const output = {
+export enum DayOfWeek {
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+  Sunday,
+}
+
+/**
+ * https://schema.org/Schedule
+ */
+export interface ParseResult {
+  schedule: {
+    byDay?: DayOfWeek
+    byMonth?: number
+    byMonthDay?: number
+    byMonthWeek?: number
+    repeatFrequency: string
+    startDate: string
+  }
+  match: {
+    index: number
+    length: number
+    text: string
+  }
+}
+
+type ENUM_DEF = [string, number]
+type STRING_DEF = [string, string]
+
+const SCHEDULE_TRIGGER_WORDS = ['every', 'each']
+const ENUM_WORDS: ENUM_DEF[] = [
+  ['second', 2],
+  ['third', 3],
+]
+const UNIT_WORDS: STRING_DEF[] = [
+  ['day', 'D'],
+  ['week', 'W'],
+]
+
+export function parse(input: string): ParseResult | null {
+  let repeatFrequency: string | undefined
+
+  let scheduleTriggerMatch: RegExpMatchArray | undefined
+  let index = 0
+  let text = ''
+  SCHEDULE_TRIGGER_WORDS.some((word) => {
+    const match = input.match(new RegExp(`^${word} | ${word} `))
+    if (match && match[0]) {
+      scheduleTriggerMatch = match
+      text = match[0].trim()
+      index = match.index || 0
+      if (match[0].at(0) === ' ') {
+        index++
+      }
+      return true
+    }
+    return false
+  })
+  if (!scheduleTriggerMatch) {
+    return null
+  }
+
+  const enumWordMatch = ENUM_WORDS.some(([word, value]) => {
+    const match = input.match(new RegExp(` ${word} `))
+    if (match && match[0]) {
+      repeatFrequency = `P${value}`
+      text = `${text} ${match[0].trim()}`
+      return true
+    }
+    return false
+  })
+  if (!enumWordMatch) {
+    repeatFrequency = 'P1'
+  }
+
+  const unitWordMatch = UNIT_WORDS.some(([word, unit]) => {
+    const match = input.match(new RegExp(` ${word}$| ${word} `))
+    if (match && match[0]) {
+      repeatFrequency = `${repeatFrequency}${unit}`
+      text = `${text} ${match[0].trim()}`
+      return true
+    }
+    return false
+  })
+  if (!unitWordMatch) {
+    return null
+  }
+
+  if (!repeatFrequency) {
+    return null
+  }
+
+  const output: ParseResult = {
     schedule: {
-      repeatFrequency: 'P1D',
+      repeatFrequency,
       startDate: new Date('2022-01-01').toISOString(),
     },
     match: {
-      index: 0,
-      length: input.length,
-      text: input,
+      index,
+      length: text.length,
+      text,
     },
   }
   return output
---
+++ tests/index.test.ts
@@ -0,0 +1,105 @@
+import { describe, it, beforeEach, expect, vi } from 'vitest'
+import { parse, ParseResult } from '../src/parse'
+
+const TODAY = new Date('2022-01-01')
+const TEST_CASES: [string, ParseResult | null][] = [
+  ['', null],
+  ['hello', null],
+  ['everyday', null],
+  ['every secondday', null],
+  ['everysecond day', null],
+  [
+    'every day',
+    {
+      schedule: {
+        repeatFrequency: 'P1D',
+        startDate: TODAY.toISOString(),
+      },
+      match: {
+        index: 0,
+        length: 9,
+        text: 'every day',
+      },
+    },
+  ],
+  [
+    'every second day',
+    {
+      schedule: {
+        repeatFrequency: 'P2D',
+        startDate: TODAY.toISOString(),
+      },
+      match: {
+        index: 0,
+        length: 16,
+        text: 'every second day',
+      },
+    },
+  ],
+  [
+    'every third day',
+    {
+      schedule: {
+        repeatFrequency: 'P3D',
+        startDate: TODAY.toISOString(),
+      },
+      match: {
+        index: 0,
+        length: 15,
+        text: 'every third day',
+      },
+    },
+  ],
+  [
+    'every week',
+    {
+      schedule: {
+        repeatFrequency: 'P1W',
+        startDate: TODAY.toISOString(),
+      },
+      match: {
+        index: 0,
+        length: 10,
+        text: 'every week',
+      },
+    },
+  ],
+  [
+    'every second week',
+    {
+      schedule: {
+        repeatFrequency: 'P2W',
+        startDate: TODAY.toISOString(),
+      },
+      match: {
+        index: 0,
+        length: 17,
+        text: 'every second week',
+      },
+    },
+  ],
+  [
+    'on every second week do workout',
+    {
+      schedule: {
+        repeatFrequency: 'P2W',
+        startDate: TODAY.toISOString(),
+      },
+      match: {
+        index: 3,
+        length: 17,
+        text: 'every second week',
+      },
+    },
+  ],
+]
+
+describe('parse', () => {
+  beforeEach(() => {
+    vi.useFakeTimers().setSystemTime(TODAY)
+  })
+
+  it.each(TEST_CASES)('parses "%s"', (input, output) => {
+    expect(parse(input)).toEqual(output)
+  })
+})
