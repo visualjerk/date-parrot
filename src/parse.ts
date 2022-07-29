e175d2d20b5f4385dde32f9c9b775941f184d4e1
visualjerk <jb@visualjerk.de>
2022-07-29 19:21:46 +0200

    add enum and units

--- src/parse.ts
+++ src/parse.ts
@@ -57,12 +57,60 @@ type STRING_DEF = [string, string]
 
 const SCHEDULE_TRIGGER_WORDS = ['every', 'each']
 const ENUM_WORDS: ENUM_DEF[] = [
+  ['first', 1],
   ['second', 2],
   ['third', 3],
+  ['fourth', 4],
+  ['fifth', 5],
+  ['sixth', 6],
+  ['seventh', 7],
+  ['eigth', 8],
+  ['nineth', 9],
+  ['tenth', 10],
+  //
+  ['one', 1],
+  ['two', 2],
+  ['three', 3],
+  ['four', 4],
+  ['five', 5],
+  ['six', 6],
+  ['seven', 7],
+  ['eight', 8],
+  ['nine', 9],
+  ['ten', 10],
+  ['eleven', 11],
+  ['twelve', 12],
+  ['thirteen', 13],
+  ['fourteen', 14],
+  ['fifteen', 15],
+  ['sixteen', 16],
+  ['seventeen', 17],
+  ['eighteen', 18],
+  ['nineteen', 19],
+  ['twenty', 20],
+  ['twentyone', 21],
+  ['twentytwo', 22],
+  ['twentythree', 23],
+  ['twentyfour', 24],
+  ['twentyfive', 25],
+  ['twentysix', 26],
+  ['twentyseven', 27],
+  ['twentyeight', 28],
+  ['twentynine', 29],
+  ['thirty', 30],
+  //
+  ['1st', 1],
+  ['2nd', 2],
+  ['3rd', 3],
 ]
 const UNIT_WORDS: STRING_DEF[] = [
+  ['second', 's'],
+  ['minute', 'm'],
+  ['hour', 'h'],
   ['day', 'D'],
   ['week', 'W'],
+  ['month', 'M'],
+  ['year', 'Y'],
 ]
 
 export function parse(input: string): ParseResult | null {
@@ -92,22 +140,35 @@ export function parse(input: string): ParseResult | null {
     return null
   }
 
-  const enumWordMatch = ENUM_WORDS.some(([word, value]) => {
-    const match = input.match(new RegExp(`^${word} +`, 'i'))
-    if (match && match[0]) {
-      repeatFrequency = `P${value}`
-      text = `${text}${match[0]}`
-      input = input.slice(match[0].length)
-      return true
-    }
-    return false
-  })
-  if (!enumWordMatch) {
+  let enumMatch = false
+  const match = input.match(new RegExp('^(\\d+)th +', 'i'))
+  if (match && match[0]) {
+    const value = match[1]
+    repeatFrequency = `P${value}`
+    text = `${text}${match[0]}`
+    input = input.slice(match[0].length)
+    enumMatch = true
+  }
+
+  if (!enumMatch) {
+    enumMatch = ENUM_WORDS.some(([word, value]) => {
+      const match = input.match(new RegExp(`^${word} +`, 'i'))
+      if (match && match[0]) {
+        repeatFrequency = `P${value}`
+        text = `${text}${match[0]}`
+        input = input.slice(match[0].length)
+        return true
+      }
+      return false
+    })
+  }
+
+  if (!enumMatch) {
     repeatFrequency = 'P1'
   }
 
   const unitWordMatch = UNIT_WORDS.some(([word, unit]) => {
-    const match = input.match(new RegExp(`^${word}$|^${word} `, 'i'))
+    const match = input.match(new RegExp(`^${word}s?$|^${word}s? `, 'i'))
     if (match && match[0]) {
       repeatFrequency = `${repeatFrequency}${unit}`
       if (match[0].at(-1) === ' ') {
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -4,208 +4,311 @@ import { DayOfWeek, Month, parse, ParseResult } from '../src/parse'
 
 const TODAY = new Date('2022-02-04')
 const TODAY_AS_ISO = formatISO(TODAY)
-const TEST_CASES: [string, ParseResult | null][] = [
-  ['', null],
-  ['hello', null],
-  ['everyday', null],
-  ['every secondday', null],
-  ['everysecond day', null],
-  ['every once in a while a second day', null],
-  [
-    'every day',
-    {
-      schedule: {
-        repeatFrequency: 'P1D',
-        startDate: TODAY_AS_ISO,
-      },
-      match: {
-        index: 0,
-        length: 9,
-        text: 'every day',
-      },
-    },
-  ],
-  [
-    'EverY Day',
-    {
-      schedule: {
-        repeatFrequency: 'P1D',
-        startDate: TODAY_AS_ISO,
-      },
-      match: {
-        index: 0,
-        length: 9,
-        text: 'EverY Day',
-      },
-    },
-  ],
-  [
-    'every second day',
-    {
-      schedule: {
-        repeatFrequency: 'P2D',
-        startDate: TODAY_AS_ISO,
-      },
-      match: {
-        index: 0,
-        length: 16,
-        text: 'every second day',
-      },
-    },
-  ],
-  [
-    'Every SecoNd Day',
-    {
-      schedule: {
-        repeatFrequency: 'P2D',
-        startDate: TODAY_AS_ISO,
-      },
-      match: {
-        index: 0,
-        length: 16,
-        text: 'Every SecoNd Day',
-      },
-    },
-  ],
-  [
-    'every third day',
-    {
-      schedule: {
-        repeatFrequency: 'P3D',
-        startDate: TODAY_AS_ISO,
-      },
-      match: {
-        index: 0,
-        length: 15,
-        text: 'every third day',
-      },
-    },
-  ],
-  [
-    'every week',
-    {
-      schedule: {
-        repeatFrequency: 'P1W',
-        startDate: TODAY_AS_ISO,
-      },
-      match: {
-        index: 0,
-        length: 10,
-        text: 'every week',
-      },
-    },
-  ],
-  [
-    'every second week',
-    {
-      schedule: {
-        repeatFrequency: 'P2W',
-        startDate: TODAY_AS_ISO,
-      },
-      match: {
-        index: 0,
-        length: 17,
-        text: 'every second week',
-      },
-    },
-  ],
-  [
-    'every  second   week',
-    {
-      schedule: {
-        repeatFrequency: 'P2W',
-        startDate: TODAY_AS_ISO,
-      },
-      match: {
-        index: 0,
-        length: 20,
-        text: 'every  second   week',
-      },
-    },
-  ],
-  [
-    'on every second week go crazy',
-    {
-      schedule: {
-        repeatFrequency: 'P2W',
-        startDate: TODAY_AS_ISO,
-      },
-      match: {
-        index: 3,
-        length: 17,
-        text: 'every second week',
-      },
-    },
-  ],
-  [
-    'every friday',
-    {
-      schedule: {
-        repeatFrequency: 'P1W',
-        byDay: DayOfWeek.Friday,
-        // today is a firday
-        startDate: TODAY_AS_ISO,
-      },
-      match: {
-        index: 0,
-        length: 12,
-        text: 'every friday',
-      },
-    },
-  ],
-  [
-    'every monday',
-    {
-      schedule: {
-        repeatFrequency: 'P1W',
-        byDay: DayOfWeek.Monday,
-        startDate: formatISO(nextMonday(TODAY)),
-      },
-      match: {
-        index: 0,
-        length: 12,
-        text: 'every monday',
-      },
-    },
-  ],
-  [
-    'every june',
-    {
-      schedule: {
-        repeatFrequency: 'P1Y',
-        byMonth: Month.June,
-        startDate: formatISO(setDate(setMonth(TODAY, 5), 1)),
-      },
-      match: {
-        index: 0,
-        length: 10,
-        text: 'every june',
-      },
-    },
-  ],
-  [
-    'every january',
-    {
-      schedule: {
-        repeatFrequency: 'P1Y',
-        byMonth: Month.January,
-        startDate: formatISO(addYears(setDate(setMonth(TODAY, 0), 1), 1)),
-      },
-      match: {
-        index: 0,
-        length: 13,
-        text: 'every january',
-      },
-    },
-  ],
-]
 
 describe('parse', () => {
   beforeEach(() => {
     vi.useFakeTimers().setSystemTime(TODAY)
   })
 
+  const TEST_CASES: [string, ParseResult | null][] = [
+    ['', null],
+    ['hello', null],
+    ['everyday', null],
+    ['every secondday', null],
+    ['everysecond day', null],
+    ['every once in a while a second day', null],
+    [
+      'every day',
+      {
+        schedule: {
+          repeatFrequency: 'P1D',
+          startDate: TODAY_AS_ISO,
+        },
+        match: {
+          index: 0,
+          length: 9,
+          text: 'every day',
+        },
+      },
+    ],
+    [
+      'EverY Day',
+      {
+        schedule: {
+          repeatFrequency: 'P1D',
+          startDate: TODAY_AS_ISO,
+        },
+        match: {
+          index: 0,
+          length: 9,
+          text: 'EverY Day',
+        },
+      },
+    ],
+    [
+      'every second day',
+      {
+        schedule: {
+          repeatFrequency: 'P2D',
+          startDate: TODAY_AS_ISO,
+        },
+        match: {
+          index: 0,
+          length: 16,
+          text: 'every second day',
+        },
+      },
+    ],
+    [
+      'Every SecoNd Day',
+      {
+        schedule: {
+          repeatFrequency: 'P2D',
+          startDate: TODAY_AS_ISO,
+        },
+        match: {
+          index: 0,
+          length: 16,
+          text: 'Every SecoNd Day',
+        },
+      },
+    ],
+    [
+      'every third day',
+      {
+        schedule: {
+          repeatFrequency: 'P3D',
+          startDate: TODAY_AS_ISO,
+        },
+        match: {
+          index: 0,
+          length: 15,
+          text: 'every third day',
+        },
+      },
+    ],
+    [
+      'every week',
+      {
+        schedule: {
+          repeatFrequency: 'P1W',
+          startDate: TODAY_AS_ISO,
+        },
+        match: {
+          index: 0,
+          length: 10,
+          text: 'every week',
+        },
+      },
+    ],
+    [
+      'every second week',
+      {
+        schedule: {
+          repeatFrequency: 'P2W',
+          startDate: TODAY_AS_ISO,
+        },
+        match: {
+          index: 0,
+          length: 17,
+          text: 'every second week',
+        },
+      },
+    ],
+    [
+      'every  second   week',
+      {
+        schedule: {
+          repeatFrequency: 'P2W',
+          startDate: TODAY_AS_ISO,
+        },
+        match: {
+          index: 0,
+          length: 20,
+          text: 'every  second   week',
+        },
+      },
+    ],
+    [
+      'on every second week go crazy',
+      {
+        schedule: {
+          repeatFrequency: 'P2W',
+          startDate: TODAY_AS_ISO,
+        },
+        match: {
+          index: 3,
+          length: 17,
+          text: 'every second week',
+        },
+      },
+    ],
+    [
+      'every friday',
+      {
+        schedule: {
+          repeatFrequency: 'P1W',
+          byDay: DayOfWeek.Friday,
+          // today is a firday
+          startDate: TODAY_AS_ISO,
+        },
+        match: {
+          index: 0,
+          length: 12,
+          text: 'every friday',
+        },
+      },
+    ],
+    [
+      'every monday',
+      {
+        schedule: {
+          repeatFrequency: 'P1W',
+          byDay: DayOfWeek.Monday,
+          startDate: formatISO(nextMonday(TODAY)),
+        },
+        match: {
+          index: 0,
+          length: 12,
+          text: 'every monday',
+        },
+      },
+    ],
+    [
+      'every june',
+      {
+        schedule: {
+          repeatFrequency: 'P1Y',
+          byMonth: Month.June,
+          startDate: formatISO(setDate(setMonth(TODAY, 5), 1)),
+        },
+        match: {
+          index: 0,
+          length: 10,
+          text: 'every june',
+        },
+      },
+    ],
+    [
+      'every january',
+      {
+        schedule: {
+          repeatFrequency: 'P1Y',
+          byMonth: Month.January,
+          startDate: formatISO(addYears(setDate(setMonth(TODAY, 0), 1), 1)),
+        },
+        match: {
+          index: 0,
+          length: 13,
+          text: 'every january',
+        },
+      },
+    ],
+  ]
+
   it.each(TEST_CASES)('parses "%s"', (input, output) => {
     expect(parse(input)).toEqual(output)
+  })
+
+  describe('triggers', () => {
+    const TEST_CASES: string[] = ['each', 'every']
+
+    it.each(TEST_CASES)('triggers on "%s"', (triggerWord) => {
+      const input = `${triggerWord} day`
+      const output = `P1D`
+      expect(parse(input)?.schedule.repeatFrequency).toEqual(output)
+    })
+  })
+
+  describe('enumaration', () => {
+    const TEST_CASES: [string, number][] = [
+      ['first', 1],
+      ['second', 2],
+      ['third', 3],
+      ['fourth', 4],
+      ['fifth', 5],
+      ['sixth', 6],
+      ['seventh', 7],
+      ['eigth', 8],
+      ['nineth', 9],
+      ['tenth', 10],
+
+      ['one', 1],
+      ['two', 2],
+      ['three', 3],
+      ['four', 4],
+      ['five', 5],
+      ['six', 6],
+      ['seven', 7],
+      ['eight', 8],
+      ['nine', 9],
+      ['ten', 10],
+      ['eleven', 11],
+      ['twelve', 12],
+      ['thirteen', 13],
+      ['fourteen', 14],
+      ['fifteen', 15],
+      ['sixteen', 16],
+      ['seventeen', 17],
+      ['eighteen', 18],
+      ['nineteen', 19],
+      ['twenty', 20],
+      ['twentyone', 21],
+      ['twentytwo', 22],
+      ['twentythree', 23],
+      ['twentyfour', 24],
+      ['twentyfive', 25],
+      ['twentysix', 26],
+      ['twentyseven', 27],
+      ['twentyeight', 28],
+      ['twentynine', 29],
+      ['thirty', 30],
+
+      ['1st', 1],
+      ['2nd', 2],
+      ['3rd', 3],
+      ['4th', 4],
+      ['5th', 5],
+      ['6th', 6],
+      ['7th', 7],
+      ['8th', 8],
+      ['9th', 9],
+      ['10th', 10],
+      ['11th', 11],
+      ['12th', 12],
+      ['28th', 28],
+      ['300th', 300],
+    ]
+
+    it.each(TEST_CASES)('parses "%s" as "%s"', (enumWord, value) => {
+      const input = `every ${enumWord} day`
+      const output = `P${value}D`
+      expect(parse(input)?.schedule.repeatFrequency).toEqual(output)
+    })
+  })
+
+  describe('units', () => {
+    const TEST_CASES: [string, string][] = [
+      ['second', 's'],
+      ['minute', 'm'],
+      ['hour', 'h'],
+      ['day', 'D'],
+      ['week', 'W'],
+      ['month', 'M'],
+      ['year', 'Y'],
+      ['seconds', 's'],
+      ['minutes', 'm'],
+      ['hours', 'h'],
+      ['days', 'D'],
+      ['weeks', 'W'],
+      ['months', 'M'],
+      ['year', 'Y'],
+    ]
+
+    it.each(TEST_CASES)('parses "%s" as "%s"', (unitWord, value) => {
+      const input = `every second ${unitWord}`
+      const output = `P2${value}`
+      expect(parse(input)?.schedule.repeatFrequency).toEqual(output)
+    })
   })
 })
