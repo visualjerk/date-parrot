f3e703a8a8b6dc3a0e53be83b9fb2670ad047671
visualjerk <jb@visualjerk.de>
2022-07-31 19:50:32 +0200

    simplify test cases

--- tests/parse-date.test.ts
+++ tests/parse-date.test.ts
@@ -5,192 +5,51 @@ import { parseDate, ParseDateResult } from '../lib'
 const TODAY = new Date('2022-02-04 01:00:00')
 const TODAY_AS_ISO = formatISO(TODAY)
 
-function createResult(
-  date: Date,
-  text: string,
-  index: number = 0
-): ParseDateResult {
-  return {
-    date: formatISO(date),
-    match: {
-      index,
-      length: text.length,
-      text,
-    },
-  }
-}
-
 describe('parseDate', () => {
   beforeEach(() => {
     vi.useFakeTimers().setSystemTime(TODAY)
   })
 
-  const TEST_CASES: [string | null, ParseDateResult | null][] = [
+  const TEST_CASES: [string | null, null | Date, string?, number?][] = [
     [null, null],
     ['', null],
     ['hello', null],
     ['heytoday', null],
     ['todayson', null],
     ['hey todayson', null],
-    [
-      'today',
-      {
-        date: TODAY_AS_ISO,
-        match: {
-          index: 0,
-          length: 5,
-          text: 'today',
-        },
-      },
-    ],
-    [
-      'tomorrow',
-      {
-        date: formatISO(addDays(TODAY, 1)),
-        match: {
-          index: 0,
-          length: 8,
-          text: 'tomorrow',
-        },
-      },
-    ],
-    [
-      'tame a bear tomorrow',
-      {
-        date: formatISO(addDays(TODAY, 1)),
-        match: {
-          index: 12,
-          length: 8,
-          text: 'tomorrow',
-        },
-      },
-    ],
-    [
-      'tomorrow tame a bear',
-      {
-        date: formatISO(addDays(TODAY, 1)),
-        match: {
-          index: 0,
-          length: 8,
-          text: 'tomorrow',
-        },
-      },
-    ],
-    [
-      'yesterday',
-      {
-        date: formatISO(addDays(TODAY, -1)),
-        match: {
-          index: 0,
-          length: 9,
-          text: 'yesterday',
-        },
-      },
-    ],
-    [
-      'friday',
-      {
-        date: TODAY_AS_ISO,
-        match: {
-          index: 0,
-          length: 6,
-          text: 'friday',
-        },
-      },
-    ],
-    [
-      'saturday',
-      {
-        date: formatISO(addDays(TODAY, 1)),
-        match: {
-          index: 0,
-          length: 8,
-          text: 'saturday',
-        },
-      },
-    ],
-    [
-      'monday',
-      {
-        date: formatISO(addDays(TODAY, 3)),
-        match: {
-          index: 0,
-          length: 6,
-          text: 'monday',
-        },
-      },
-    ],
-    [
-      'next saturday',
-      {
-        date: formatISO(addDays(TODAY, 1)),
-        match: {
-          index: 0,
-          length: 13,
-          text: 'next saturday',
-        },
-      },
-    ],
-    [
-      'next friday',
-      {
-        date: formatISO(addDays(TODAY, 7)),
-        match: {
-          index: 0,
-          length: 11,
-          text: 'next friday',
-        },
-      },
-    ],
-    [
-      'may',
-      {
-        date: formatISO(new Date('2022-05-01 01:00:00')),
-        match: {
-          index: 0,
-          length: 3,
-          text: 'may',
-        },
-      },
-    ],
-    [
-      'january',
-      {
-        date: formatISO(new Date('2023-01-01 01:00:00')),
-        match: {
-          index: 0,
-          length: 7,
-          text: 'january',
-        },
-      },
-    ],
-    [
-      'february',
-      {
-        date: formatISO(new Date('2023-02-01 01:00:00')),
-        match: {
-          index: 0,
-          length: 8,
-          text: 'february',
-        },
-      },
-    ],
-    [
-      'next february',
-      {
-        date: formatISO(new Date('2023-02-01 01:00:00')),
-        match: {
-          index: 0,
-          length: 13,
-          text: 'next february',
-        },
-      },
-    ],
+    ['today', TODAY],
+    ['tomorrow', addDays(TODAY, 1)],
+    ['tame a bear tomorrow', addDays(TODAY, 1), 'tomorrow', 12],
+    ['tomorrow tame a bear', addDays(TODAY, 1), 'tomorrow'],
+    ['yesterday', addDays(TODAY, -1)],
+    ['friday', TODAY],
+    ['saturday', addDays(TODAY, 1)],
+    ['monday', addDays(TODAY, 3)],
+    ['next saturday', addDays(TODAY, 1)],
+    ['next friday', addDays(TODAY, 7)],
+    ['may', new Date('2022-05-01 01:00:00')],
+    ['january', new Date('2023-01-01 01:00:00')],
+    ['february', new Date('2023-02-01 01:00:00')],
+    ['next february', new Date('2023-02-01 01:00:00')],
   ]
 
-  it.each(TEST_CASES)('parses "%s"', (input, output) => {
-    expect(parseDate(input as string)).toEqual(output)
-  })
+  it.each(TEST_CASES)(
+    'parses "%s"',
+    (input, date, text = input || '', index = 0) => {
+      const output =
+        date === null
+          ? null
+          : {
+              date: formatISO(date),
+              match: {
+                index,
+                length: text.length,
+                text,
+              },
+            }
+      expect(parseDate(input as string)).toEqual(output)
+    }
+  )
 
   describe('german', () => {
     const TEST_CASES: [string | null, ParseDateResult | null][] = [
